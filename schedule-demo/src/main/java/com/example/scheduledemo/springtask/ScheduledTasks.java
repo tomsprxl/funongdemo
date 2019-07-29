adc62896bfcced9046aadaa6039b53f1b5919a0b
xuliang <18656756681@163.com>
2019-07-29 16:42:36 +0800

    add zksdk-demo

---
+++ schedule-demo/src/main/java/com/example/scheduledemo/springtask/ScheduledTasks.java
@@ -0,0 +1,11 @@
+package com.example.scheduledemo.springtask;
+
+/**
+ * 描述:
+ *
+ * @author xuliang
+ * @create 2019-07-29 11:25
+ */
+public class ScheduledTasks {
+
+}
---
+++ zksdk-demo/src/main/java/com/example/zksdkdemo/ZksdkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.zksdkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZksdkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZksdkDemoApplication.class, args);
+    }
+
+}
