fb756dea5564c994f064923dd6c09fd16d54095d
xuliang <18656756681@163.com>
2019-07-31 09:37:15 +0800

    add zkreader

---
+++ zkreader/src/main/java/com/fn/zkreader/ZkreaderApplication.java
@@ -0,0 +1,13 @@
+package com.fn.zkreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkreaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkreaderApplication.class, args);
+    }
+
+}
